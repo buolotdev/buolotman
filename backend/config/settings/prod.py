cd03a7e5d896734b2d530419da899f7a6791eadb
ALI Ahmad ramzan <128125008+Aliahma24@users.noreply.github.com>
2026-08-01 03:49:02 +0500

    Trigger amplify build

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -4,10 +4,13 @@ DEBUG = False
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 import dj_database_url
+DATABASE_URL = config('DATABASE_URL', default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+ssl_require = DATABASE_URL.startswith('postgres')
 DATABASES = {
     'default': dj_database_url.config(
+        default=DATABASE_URL,
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=ssl_require,
     )
 }
 
